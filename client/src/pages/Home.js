ebeb9dcb7851225966c843f6325d6e783869571f
roshniipatel <roshnicpatel00@gmail.com>
2023-06-12 19:55:08 -0500

    flower emoji added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     <div className="home-container">
       <div className="hero-section">
         <div className="hero-overlay">
-          <h1>Welcome to Quell</h1>
+          <h1>✿ Welcome to Quell ✿</h1>
           <p>A mental health support community where you can find resources, connect with others, and seek support on your mental health journey.</p>
           <div className="buttons">
             <Link to="/login" className="button">Sign In</Link>
@@ -16,7 +16,7 @@ const Home = () => {
         </div>
       </div>
       <div className="content-section">
-        <h2>Explore, Connect, and Thrive</h2>
+        <h2>✿ Explore, Connect, and Thrive ✿</h2>
         <p>Discover a supportive community where you can share your experiences, learn from others, and find helpful resources to support your mental well-being.</p>
         {/* Add additional content sections as needed */}
       </div>
